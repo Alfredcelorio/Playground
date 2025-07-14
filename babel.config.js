6cafe621256b96068024d3e3f170c5130528410b
DrNFox <biltd.88@gmail.com>
2025-07-14 03:03:44 -0400

    fix: babel and jsEngine

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo'],
     plugins: [
-      // Ensure all three plugins are present and have the same loose mode
       ['@babel/plugin-transform-class-properties', { loose: false }],
       ['@babel/plugin-transform-private-methods', { loose: false }],
       ['@babel/plugin-transform-private-property-in-object', { loose: false }],
-      'react-native-reanimated/plugin', // <- Make sure this is last
+      'react-native-reanimated/plugin',
     ],
   };
 };
